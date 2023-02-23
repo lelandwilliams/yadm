f8ea6e32d8382271a6ee489ad926e36b6b495a58
Leland Williams <will4379@umn.edu>
2023-02-23 10:58:34 -0600

    initial adds

---
+++ .config/nvim/lua/lees/init.lua
@@ -0,0 +1,7 @@
+vim.o.termguicolors = true
+vim.o.guicursor = ""
+require("lees.packer")
+require("lees.remap")
+print("I love Steffie")
+
+
---
+++ .config/nvim/lua/lees/packer.lua
@@ -0,0 +1,60 @@
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Provides telesecope, a fuzzy finder, 2/16/2023
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- rose-pine background colors
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  require("rose-pine").setup({
+			  dark_variant = 'moon'
+		  })
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
+  -- Treesitter provides syntax parsing
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  -- Harpoon provides file bookmarks. 2/21/23
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  -- fugitive handles git commands
+  use('tpope/vim-fugitive')
+
+  -- lsp-zero
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {'williamboman/mason.nvim'},           -- Optional
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},         -- Required
+		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+		  {'hrsh7th/cmp-buffer'},       -- Optional
+		  {'hrsh7th/cmp-path'},         -- Optional
+		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},             -- Required
+		  {'rafamadriz/friendly-snippets'}, -- Optional
+	  }
+  } 
+end)
